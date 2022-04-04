84264465ff676e04d55a9363028d7e4bb44a0ffe
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-04-04 10:09:42 +0200

    premier pas

---
+++ app/src/main/java/education/cccp/tp11listview/models/Person.java
@@ -0,0 +1,58 @@
+package education.cccp.tp11listview.models;
+
+import androidx.annotation.NonNull;
+
+import java.io.Serializable;
+
+public class Person implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private String firstName;
+    private String lastName;
+
+    public Person(
+            Integer id,
+            String firstName,
+            String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        //noinspection StringBufferReplaceableByString
+        return new StringBuilder("Person{")
+                .append("id=").append(id)
+                .append(", firstName='").append(firstName).append('\'')
+                .append(", lastName='").append(lastName).append('\'')
+                .append('}')
+                .toString();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
